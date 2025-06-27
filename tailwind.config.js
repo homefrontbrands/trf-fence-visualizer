696b5a105b38b7e5e35d352a14286a14f99e718a
homefrontbrands <137557415+homefrontbrands@users.noreply.github.com>
2025-06-27 17:49:03 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        primary: '#445d48',
+        accent: '#a87c4f',
+        neutral: '#f2f2f2',
+        dark: '#333333'
+      }
+    }
+  },
   plugins: [],
 }
